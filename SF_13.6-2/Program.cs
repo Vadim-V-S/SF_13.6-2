12d54b01e6b743123b553609401f2108db5fd22b
Vadim-V-S <vaddimsav@gmail.com>
2021-07-16 14:56:45 +0500

    Добавьте файлы проекта.

---
+++ SF_13.6-2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SF_13._6_2
+{
+    class Program
+    {
+        static Text Text = new Text(@"C:/Users/Savinykh/Desktop/С/Text1.txt");
+
+
+        static void Main(string[] args)
+        {
+
+            Text.GetTextToString();
+            Text.PutUniqueWordsToDictionary(3); // аргументом отсекаем предлоги, местоимения, наречия по длине слова (о - учитывает всё)
+           
+            CountWords();
+
+            PrintResult(Text.UniqueWordDictionary.OrderBy(x => -x.Value).ToDictionary(x => x.Key, x => x.Value));
+
+
+            Console.ReadKey();
+        }
+
+
+        static void CountWords()
+        {
+            foreach (var word in Text.EntireText)
+                if (Text.UniqueWordDictionary.ContainsKey(word)) Text.UniqueWordDictionary[word]++;
+        }
+
+
+        static void PrintResult(Dictionary<string,int> FrequentWord)
+        {
+            int i = 0;
+            foreach (var word in FrequentWord)
+            {
+                Console.WriteLine("Слово -{0}- встречается {1} раз", word.Key, word.Value);
+                i++;
+                if (i == 4) break;
+            }
+        }
+    }
+}
---
+++ SF_13.6-2/Text.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SF_13._6_2
+{
+    class Text
+    {
+        public string[] EntireText;
+        string path;
+        public Dictionary<string, int> UniqueWordDictionary;
+
+        public Text( string path)
+        {
+            this.path = path;
+        }
+
+         public void GetTextToString()
+        {
+            string str = File.ReadAllText(path);
+            char[] delimeters = { ' ', '\r', '\n', '–' };
+            EntireText = str.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+
+         public void PutUniqueWordsToDictionary(int cutter)
+        {
+            var UniqueWordlist = new HashSet<string>();
+            UniqueWordDictionary = new Dictionary<string, int>();
+
+            foreach (var word in EntireText)
+                UniqueWordlist.Add(word);
+
+            foreach (var word in UniqueWordlist)
+            {
+                if (word.Length > cutter)
+                    UniqueWordDictionary.Add(word, 0);
+            }
+        }
+    }
+}
